d225e06ac6587972f99f3691afe8eea0574cc2f9
Scarlet Shao <scarletshao@gmail.com>
2021-03-10 22:57:12 +0000

    add client file

--- play.js
+++ play.js
@@ -1,19 +1,8 @@
 const net = require('net');
-
+const { connect } = require('./client');
 /**
  * Establishes connection with the game server
  */
-const connect = function() {
-  const conn = net.createConnection({ 
-    host: '135.23.222.131',
-    port: 50542,
-    password:820368
-  });
-  // interpret incoming data as text
-  conn.setEncoding('utf8'); 
-  console.log("you ded cuz you idled");
-  return conn;
-}
 
 console.log('Connecting ...');
 connect();
